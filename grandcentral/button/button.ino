2bf5ff13c8e770e052065a017a74a41277989782
AntonSangho <sanghoemail@gmail.com>
2022-10-02 23:11:06 +0900

    버튼

--- grandcentral/button/button.ino
+++ grandcentral/button/button.ino
@@ -9,12 +9,12 @@ void setup() {
 
 void loop() {
     int val = digitalRead(inputPin); //입력값을 읽는다.
-    if ( val == HIGH )
+    if ( val == HIGH ) 
     {
-        digitalWrite(ledPin, HIGH); //LED를 끈다.
+        digitalWrite(ledPin, LOW); //버튼 입력이 없을때는 HIGH 이므로 LED를 끈다.
     }
     else
     {
-        digitalWrite(ledPin, LOW); //LED를 켠다.
+        digitalWrite(ledPin, HIGH); //버튼 입력이 있을 때는 LOW 이므로 LED를 켠다.
     }
 }
